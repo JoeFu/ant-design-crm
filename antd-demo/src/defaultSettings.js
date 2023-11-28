c7cc71f7daaedb473be7379aa04334b1a41cadba
JoeFu <fuzeqi@gmail.com>
2023-11-28 20:06:09 +1030

    init

---
+++ antd-demo/src/defaultSettings.js
@@ -0,0 +1,9 @@
+export default{
+  "fixSiderbar": true,
+  "layout": "mix",
+  "splitMenus": false,
+  "navTheme": "light",
+  "contentWidth": "Fluid",
+  "colorPrimary": "#1677FF",
+  "siderMenuType": "sub"
+};
